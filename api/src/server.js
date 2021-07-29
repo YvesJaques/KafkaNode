a3adc5734a38a66f6e57ab48b044073cef056dc4
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-07-29 11:46:58 -0300

    feat: Certificate response to server

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Kafka } from 'kafkajs';
+import { Kafka, logLevel } from 'kafkajs';
 
 import routes from '../src/routes.js';
 
@@ -9,13 +9,15 @@ const app = express();
 const kafka = new Kafka({
     clientId: 'api',
     brokers: ['localhost:9092'],
+    logLevel: logLevel.WARN,
     retry: {
         initialRetryTime: 300,
         retries: 10
-    }
+    },
 });
 
 const producer = kafka.producer()
+const consumer = kafka.consumer({groupId: 'certificate-group-receiver'})
 
 //Provides producer for all routes
 app.use((req, res, next) => {
@@ -29,6 +31,15 @@ app.use(routes)
 
 async function run() {
     await producer.connect()
+    await consumer.connect()
+
+    await consumer.subscribe({ topic: 'certification-response' })
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log('Resposta:', String(message.value));
+        },
+    })    
 
     app.listen(3333);
 }
--- certification/src/index.js
+++ certification/src/index.js
@@ -9,13 +9,28 @@ const kafka = new Kafka({
 const topic = 'issue-certificate'
 const consumer = kafka.consumer({ groupId: 'certificate-group' })
 
+const producer = kafka.producer();
+
 async function run() {
     await consumer.connect()
+    await producer.connect()
     await consumer.subscribe({ topic })
     await consumer.run({
-        eachMessage: async ({ topic, partition, message }) => {
+        eachMessage: async ({ topic, partition, message }) => {            
             const prefix  = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
             console.log(`- ${prefix} ${message.key}#${message.value}`)
+
+            const payload = JSON.parse(message.value);
+
+            // setTimeout(() => {
+            producer.send({
+                topic: 'certification-response',
+                messages: [
+                    { value: `Certificado do usuário #${payload.user.name} do curso #${payload.course} gerado!`}
+                ]
+            })
+            // }, 3000);
+
         },
     })
 }
